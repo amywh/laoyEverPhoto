856f20d1c8e18d2463ab7d49193620e0aeb8373e
amywh <86214922+amywh@users.noreply.github.com>
2021-08-01 18:26:13 +0800

    Initial commit

---
+++ internal/model/model.go
@@ -0,0 +1,42 @@
+package model
+
+import "fmt"
+
+type (
+	CommonResponse struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	}
+
+	AuthResponse struct {
+		CommonResponse
+		Data struct {
+			Token string `json:"token"`
+		} `json:"data"`
+	}
+
+	SpaceReward int64
+
+	CheckinResult struct {
+		Success        bool        `json:"checkin_result"`
+		Continuity     int         `json:"continuity"`
+		TotalReward    SpaceReward `json:"total_reward"`
+		TomorrowReward SpaceReward `json:"tomorrow_reward"`
+	}
+
+	CheckinResponse struct {
+		CommonResponse
+		Data *CheckinResult `json:"data"`
+	}
+
+	PushResponse struct {
+		Errno  int    `json:"errno"`
+		Errmsg string `json:"errmsg"`
+	}
+)
+
+const MB = 1 << 20
+
+func (sr SpaceReward) String() string {
+	return fmt.Sprintf("%dMB", sr/MB)
+}
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/winterssy/EverPhotoCheckin/internal/client"
+	"github.com/winterssy/EverPhotoCheckin/internal/push"
+)
+
+const (
+	EnvEverPhotoMobile   = "EverPhotoMobile"
+	EnvEverPhotoPassword = "EverPhotoPassword"
+	EnvEverPhotoToken    = "EverPhotoToken"
+	EnvServerChanKey     = "ServerChanKey"
+)
+
+var (
+	_mobile   string
+	_password string
+	_token    string
+)
+
+func init() {
+	flag.StringVar(&_mobile, "mobile", "", "your mobile phone number")
+	flag.StringVar(&_password, "password", "", "your password")
+	flag.StringVar(&_token, "token", "", "your token")
+}
+
+func valueOrDefault(value, def string) string {
+	if value != "" {
+		return value
+	}
+	return def
+}
+
+func createBot() (bot *client.Bot, err error) {
+	_token = valueOrDefault(_token, os.Getenv(EnvEverPhotoToken))
+	if _token != "" {
+		bot = client.NewWithToken(_token)
+		return
+	}
+
+	_mobile = valueOrDefault(_mobile, os.Getenv(EnvEverPhotoMobile))
+	_password = valueOrDefault(_password, os.Getenv(EnvEverPhotoPassword))
+	bot, err = client.New(_mobile, _password)
+	return
+}
+
+var _scKey = os.Getenv(EnvServerChanKey)
+
+func pushMessage(ok bool, desp string) {
+	if _scKey == "" {
+		return
+	}
+
+	const (
+		_textSuccess = "时光相册签到成功通知"
+		_textFailure = "时光相册签到失败通知"
+	)
+	var err error
+	if ok {
+		err = push.Push(_scKey, _textSuccess, desp)
+	} else {
+		err = push.Push(_scKey, _textFailure, "错误详情 >> "+desp)
+	}
+
+	if err != nil {
+		log.Print("Server酱推送失败：" + err.Error())
+	}
+}
+
+func errDesp(msg string, err error) string {
+	return msg + "：" + err.Error()
+}
+
+func main() {
+	flag.Parse()
+
+	var desp string
+	bot, err := createBot()
+	if err != nil {
+		desp = errDesp("登录失败", err)
+		pushMessage(false, desp)
+		log.Fatal("【时光相册】" + desp)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	cr, err := bot.Checkin(ctx)
+	if err != nil {
+		desp = errDesp("签到失败", err)
+		pushMessage(false, desp)
+		log.Fatal("【时光相册】" + desp)
+	}
+
+	desp = fmt.Sprintf("你已连续签到%d天，累计获得空间%s，明天可白嫖%s，请继续保持(￣▽￣)",
+		cr.Continuity, cr.TotalReward, cr.TomorrowReward)
+	pushMessage(true, desp)
+	log.Print("【时光相册】" + desp)
+}
